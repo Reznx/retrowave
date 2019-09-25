3aefb2a64dc32c4562abfae78db67d5ed72c6876
Reznx <48242089+Reznx@users.noreply.github.com>
2019-09-25 17:12:23 +0500

    Restyle home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,15 @@
       <div class="centerer">
         <p class="text two">Do what you love</p>
       </div>
+      <div class="headphones">
+        <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" width="40" height="40">
+          <path
+            d="M16 8A6 6 0 1 0 4 8v11H2a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2V8a8 8 0 1 1 16 0v3a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-2V8zm-4 2h3v10h-3V10zm-7 0h3v10H5V10z"
+            fill="white"
+          />
+        </svg>
+        <p class="headphones__text">Рекомендуется надеть наушники</p>
+      </div>
     </div>
     <div class="button">
       <router-link to="/about" class="link">DO MORE</router-link>
